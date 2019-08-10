4530b1153ea3636c38a3371fa4009915b4282980
changpw <1589273884@qq.com>
2019-08-10 15:02:53 +0800

    all update

--- vue/vue.js
+++ vue/vue.js
@@ -5,9 +5,12 @@
 var vm = new Vue({
     el : '#app',
     data :{
+        datetime:"",
+        name:[],
+        activeColor:"green",
         watches :'',
         msg : 'Hello Vue',
-        use : '',
+        use : "true",
         message:'<h1>Vue</h1>',
         lastName : '',
         firstName : '',
@@ -33,7 +36,19 @@ var vm = new Vue({
     methods :{
         show : function () {
             alert(this.use)
+        },
+        showName: function () {
+            var reg=/^1(3|4|5|6|7|8|9)\d{9}$/;
+            console.log(this.name)
+            console.log(this.datetime)
+            if(reg.test(this.name)){
+                alert('格式正确');
+            }
+            else {
+                alert(this.datetime)
+            }
         }
+
         
     },
     computed:{
